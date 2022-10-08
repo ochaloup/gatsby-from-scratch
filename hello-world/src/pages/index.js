6abdd6af0d9e10c4b91a56d729bb4c7806a1ffd4
Ondra Chaloupka <chalda@chainkeepers.io>
2022-10-08 18:21:00 +0200

    playing with graphql

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
-import { useSiteMetadata } from '../hooks/useSiteMetadata';
+import { Layout } from '../components/Layout';
 
-const StyledH1 = styled.h1`
-  color: rebeccapurple;
-`;
 
 const Named = () => {
-  const { title, description } = useSiteMetadata();
   return (
     <>
-      <StyledH1>{title}</StyledH1>
-      <p>{description}</p>
+      <Layout />
     </>
   );
 };
