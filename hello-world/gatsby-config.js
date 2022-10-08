7666cc0c29cd4cd823811b0b8611c117bbd4525f
Ondra Chaloupka <chalda@chainkeepers.io>
2022-10-08 17:43:41 +0200

    Using doc and moving with layouts

--- hello-world/gatsby-config.js
+++ hello-world/gatsby-config.js
@@ -1,9 +1,19 @@
+const siteMetadata = {
+  title: `The Localhost Blog`,
+  description: `This is my coding blog where I write about my coding journey.`,
+}
+
 module.exports = {
-    siteMetadata: {
-      title: `The Localhost Blog`,
-      description: `This is my coding blog where I write about my coding journey.`,
-    },
+    siteMetadata: siteMetadata,
     plugins: [
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/posts`,
+          name: `posts`,
+        },
+      },
+      `gatsby-plugin-styled-components`,
       {
         resolve: `gatsby-plugin-mdx`,
         options: {
@@ -11,10 +21,12 @@ module.exports = {
         },
       },
       {
-        resolve: `gatsby-source-filesystem`,
+        resolve: `gatsby-transformer-asciidoc`,
         options: {
-          path: `${__dirname}/posts`,
-          name: `posts`,
+          attributes: {
+            showtitle: true,
+          },
+          extensions: [`.adoc`],
         },
       },
     ],
--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useSiteMetadata } from '../hooks/useSiteMetadata';
 
-export default () => {
-  return <h1>Hello World!</h1>;
+const StyledH1 = styled.h1`
+  color: rebeccapurple;
+`;
+
+const Named = () => {
+  const { title, description } = useSiteMetadata();
+  return (
+    <>
+      <StyledH1>{title}</StyledH1>
+      <p>{description}</p>
+    </>
+  );
 };
+export default Named;
