9842383b60ca9b3793b3b6638c5763005588526f
Ondra Chaloupka <chalda@chainkeepers.io>
2022-10-08 19:01:01 +0200

    removing debug message

--- hello-world/gatsby-node.js
+++ hello-world/gatsby-node.js
@@ -2,7 +2,6 @@ const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
-  console.log(`internal type: ${node.internal.type}`)
   if (node.internal.type === `Mdx` || node.internal.type === `Asciidoc`) {
     const value = createFilePath({ node, getNode });
     createNodeField({
